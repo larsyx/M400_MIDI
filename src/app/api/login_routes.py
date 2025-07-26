b62ba637277383b9831cc88036e9e6266b86a0d4
GABRIELE\gabri <gabriele.2000.02@gmail.com>
2025-07-26 20:19:23 +0200

    improve UX

--- src/app/api/login_routes.py
+++ src/app/api/login_routes.py
@@ -3,10 +3,13 @@ from fastapi.responses import HTMLResponse, FileResponse, RedirectResponse
 from app.auth.security import get_current_user
 from app.services.auth_service import AuthService
 from fastapi.templating import Jinja2Templates
+from app.dao.user_dao import UserDAO
+from app.auth.auth import verify_token  
+
 import os
 
 router = APIRouter()
-
+userDAO = UserDAO()
 auth_service = AuthService()
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -16,7 +19,19 @@ templates = Jinja2Templates(directory=TEMPLATE_DIR)
 
 @router.get("/", response_class=HTMLResponse)
 async def root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    token = request.cookies.get("access_token")
+
+    if not token:
+        return templates.TemplateResponse("index.html", {"request": request})    
+
+    payload = verify_token(token)
+    if payload is None:
+        return templates.TemplateResponse("index.html", {"request": request})   
+    elif userDAO.get_user_by_username(payload["sub"]) == None:
+       return templates.TemplateResponse("index.html", {"request": request})   
+
+    return auth_service.home(payload["sub"])
+    
 
 @router.post("/login")
 async def login(request : Request, username: str = Form(...)):
@@ -34,3 +49,10 @@ async def home(request: Request):
     user_data = get_current_user(request)
     
     return auth_service.home(user_data["sub"])
+
+
+@router.get("/logout", response_class=RedirectResponse)
+async def logout(request: Request):
+    response = RedirectResponse(url="/", status_code=302)
+    response.delete_cookie(key="access_token")
+    return response
